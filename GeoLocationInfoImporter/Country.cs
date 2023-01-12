8205a9140af13e9bebe1b929e30b7f9e5fc8405f
Tiago Rafael Silva <tiago.silva@equusoft.com>
2023-01-12 13:50:49 +0000

    add countries

---
+++ GeoLocationInfoImporter/Country.cs
@@ -0,0 +1,21 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoLocationInfoImporter
+{
+    public class Country
+    {
+        [Index(1)]
+        public string Name { get; set; }
+
+        [Index(2)]
+        public string Code { get; set; }
+
+        [Index(3)]
+        public string IsoCode2Character { get; set; }
+    }
+}
---
+++ GeoLocationInfoImporter/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+using CsvHelper;
+using GeoLocationInfoImporter;
+
+Console.WriteLine("Hello, Importer!");
+
+using (var reader = new StreamReader(@"..\..\..\Data\countries.csv"))
+using (StreamWriter writer = new StreamWriter("countries_output.txt"))
+using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+{
+    var records = csv.GetRecords<Country>().ToList();
+    foreach (var country in records)
+    {
+        writer.WriteLine($"INSERT INTO Country(Name, Code, IsoCode2Character)VALUES('{country.Name}', '{country.Code}', '{country.IsoCode2Character}');");
+    }
+    Console.WriteLine($"We wrote {records.Count()} countries;");
+}
+Console.ReadLine();
